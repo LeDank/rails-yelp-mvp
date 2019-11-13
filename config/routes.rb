f321bfe619ff75a221102f2e6199532b1ff0f76f
Pierre Danckaert pierre.danckaert@njoy.fr <pierre.danckaert@njoy.fr>
2019-11-13 17:06:38 +0100

    ajout des liens

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# # #READ ALL
+# #   get 'restaurants', to: 'restaurants#index'
+# # #READ ONE
+# #   get 'restaurants/:id', to: 'restaurants#show'
+
+#   resources :restaurants do
+#     collection do                       # collection => no restaurant id in URL
+#       get 'top'                         # RestaurantsController#top
+#     end
+#   end
+
+resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Le Palais de Jade',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0320212223',
+    category:       'chinese'
+  },
+  {
+    name:         'Chez Werner',
+    address:      '12 rue du Belge, Tournai',
+    phone_number:  '0320212223',
+    category:       'belgian'
+  },
+  {
+    name:         'La table de Martin',
+    address:      '10 rue du Loup, Bensançon',
+    phone_number:  '0320212223',
+    category:       'french'
+  },
+  {
+    name:         'Mudra',
+    address:      '5 avenue Spyro, Paris',
+    phone_number:  '0320212223',
+    category:      'italian'
+  },
+  {
+    name:         'Yakamura',
+    address:      '12 rue du Lotus Bleu, Lille',
+    phone_number:  '0320212223',
+    category:      'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
